0f71ad8bd52f528c5bf595866b53acab10590220
Nyuykimac <macnyuyki@gmail.com>
2024-04-24 17:06:55 +0100

    new files

---
+++ learn_rust/target/debug/deps/learn_rust-e6081ff70c1aea05.d
@@ -0,0 +1,5 @@
+/home/nyuykighan/Desktop/Learn_Rust/learn_rust/target/debug/deps/liblearn_rust-e6081ff70c1aea05.rmeta: src/main.rs
+
+/home/nyuykighan/Desktop/Learn_Rust/learn_rust/target/debug/deps/learn_rust-e6081ff70c1aea05.d: src/main.rs
+
+src/main.rs:
