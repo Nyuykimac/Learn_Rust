015e71048b40061936583b9e12ccfade29f7bc41
Nyuykimac <macnyuyki@gmail.com>
2024-04-24 17:19:57 +0100

    implemented type annotation

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -7,4 +7,10 @@ fn main() {
     let test_value = test_value + 5;
 
     println!("The new value of the shadowed variable is, {}", test_value); //illustration of concept of shadowing
+
+    let number = 10; // this type is intrinsic ie it will be inferred by the compiler
+
+    let number: i32 = number + 25; // this is an explicit declaration
+
+    println!("the value of the number is : {}", number);
 }
