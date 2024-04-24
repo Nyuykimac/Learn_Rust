a292053e576524fd4ba0e4ed2976f34a2ba858a7
Nyuykimac <macnyuyki@gmail.com>
2024-04-24 18:52:31 +0100

    implemented data types

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -13,4 +13,26 @@ fn main() {
     let number: i32 = number + 25; // this is an explicit declaration
 
     println!("the value of the number is : {}", number);
+
+    //Data types in Rust. There are four main data types namely: Integer, Floating, caracter and boolean.
+
+    let int_number = 16u32;
+    let int_signed_number = 23u32;
+    let sum: u32 = int_number + int_signed_number;
+
+    println!("the sum is: {}", sum);
+
+    // bool type
+
+    let check = 1 > 34;
+
+    println!(" is 1>34?: {}", check);
+
+    //string, there several types of strings but we focus on two types: str and String
+
+    let name = "Mark";
+
+    let last_name: &str = "Ngoran";
+
+    println!("the name is : {}, last name is: {}", name, last_name);
 }
