5c8298d1c967651d50bbf2d8aca6cedafb5f36c2
Nyuykimac <macnyuyki@gmail.com>
2024-04-24 17:04:38 +0100

    implemented concept of shadowing

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("The first letter of the alphabet is {} and the last is {}", "A", 'Z'); //Testing Macros in rust
 
-print!("The first letter of the alphabet is {} and the last is {}", "A", 'Z'); //Testing Macros in rust
+    let test_value = 10;
 
+    let test_value = test_value * 2;
+    let test_value = test_value + 5;
+
+    println!("The new value of the shadowed variable is, {}", test_value); //illustration of concept of shadowing
 }
