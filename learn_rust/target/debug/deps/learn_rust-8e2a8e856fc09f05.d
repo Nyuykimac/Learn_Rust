543b1b12a378d9c0efd76c825bff53fde794b6e0
Nyuykimac <macnyuyki@gmail.com>
2024-04-24 16:32:16 +0100

    first compile using cargo run

---
+++ learn_rust/target/debug/deps/learn_rust-8e2a8e856fc09f05.d
@@ -0,0 +1,5 @@
+/home/nyuykighan/Desktop/Learn_Rust/learn_rust/target/debug/deps/liblearn_rust-8e2a8e856fc09f05.rmeta: src/main.rs
+
+/home/nyuykighan/Desktop/Learn_Rust/learn_rust/target/debug/deps/learn_rust-8e2a8e856fc09f05.d: src/main.rs
+
+src/main.rs:
---
+++ learn_rust/target/debug/learn_rust.d
@@ -0,0 +1,1 @@
+/home/nyuykighan/Desktop/Learn_Rust/learn_rust/target/debug/learn_rust: /home/nyuykighan/Desktop/Learn_Rust/learn_rust/src/main.rs
